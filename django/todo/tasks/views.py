4810cdec6ff0361aa68d3efcca612fd37f88e40c
Jacenty00 <jacek.kmiecik99@gmail.com>
2020-05-21 21:36:16 +0200

    Templeting skeleton for home

--- django/todo/tasks/views.py
+++ django/todo/tasks/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.models import Task
 # Create your views here.
 def home(request):
 # Render the HTML template home.html
-    return render(request, 'home.html')
+    task_list = Task.objects.all()
+    context_dict = {'tasks' : task_list}
+    return render(request, 'home.html', context_dict)
 
 def add(request):
     return render(request, 'add.html')
