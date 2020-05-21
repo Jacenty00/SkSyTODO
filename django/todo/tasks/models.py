7523c75c66d103e3792c52d80eea6ed399f489f5
Jacenty00 <jacek.kmiecik99@gmail.com>
2020-05-21 21:03:48 +0200

    some changes/

--- django/todo/tasks/models.py
+++ django/todo/tasks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    task_name = models.CharField(max_length=30) ## Nazwa taska, to o czym ten gosciu gadal ostatnio albo zostawic ID.
     task_text = models.CharField(max_length=160)
     task_progress = models.IntegerField(default=0)
     task_deadline = models.DateField()
--- django/todo/tasks/views.py
+++ django/todo/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+# Render the HTML template home.html
+    return render(request, 'home.html')
+
+def add(request):
+    return render(request, 'add.html')
+def edit(request):
+    return render(request, 'edit.html')
+def impressum(request):
+    return render(request, 'impressum.html')        
