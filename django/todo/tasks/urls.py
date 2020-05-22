c316b7c925282ec48e5f7ee2c80f9aed5753cd4a
witja46 <witja46@gmail.com>
2020-05-22 13:45:14 +0200

    admin added, Task model regestriert, you cann add new task via admin, urls taken in one place

--- django/todo/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-path('', views.home, name='home'),
-path('add/', views.add, name='add'),
-path('edit/', views.edit, name='edit'),
-path('impressum/', views.impressum, name='impressum'),
-]
--- django/todo/todo/urls.py
+++ django/todo/todo/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls'))
+    path('', views.home, name='home'),
+    path('add/', views.add, name='add'),
+    path('edit/', views.edit, name='edit'),
+    path('edit.html', views.edit, name='edit'),
+    path('impressum/', views.impressum, name='impressum'),
 
 ]
